82854f0dcb5139ec037d56582b43fc07209beb79
marcos schernetzki <marcoschernetzki@gmail.com>
2023-08-08 22:07:47 +0200

    update cluster

--- src/db/db.connect.ts
+++ src/db/db.connect.ts
@@ -4,6 +4,6 @@ import { USER, CLUSTER, PASSW } from '../config.js';
 export async function dbConnect() {
     const DBName = process.env.NODE_ENV !== 'test' ? 'Houses' : 'HousesTesting';
     let uri = `mongodb+srv://${USER}:${PASSW}`;
-    uri += `@${CLUSTER}/${DBName}?retryWrites=true&w=majority`;
+    uri += `${CLUSTER}/${DBName}?retryWrites=true&w=majority`;
     return mongoose.connect(uri);
 }
